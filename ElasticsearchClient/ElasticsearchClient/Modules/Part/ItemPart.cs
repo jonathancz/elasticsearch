8ece0474d775a38c98c9112621a1cd1fa2608269
Ethan Chen <jonathanchenzheng96@gmail.com>
2023-08-25 21:30:45 -0400

    Add ItemPart

---
+++ ElasticsearchClient/ElasticsearchClient/Modules/Part/ItemPart.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+
+namespace ElasticsearchClient.Modules.Part;
+
+public class ItemPart
+{
+    public Guid Id { get; set; }
+    public string Partcode { get; set; }
+    public string Description { get; set; }
+    public decimal Cost { get; set; }
+
+    public ItemPart(Guid id, string partcode, string description, decimal cost)
+    {
+        Id = id;
+        Partcode = partcode;
+        Description = description;
+        Cost = cost;
+    }
+}
+
+public class ItemPartFaker : Faker<ItemPart>
+{
+    public ItemPartFaker()
+    {
+        CustomInstantiator(f => new ItemPart(Guid.NewGuid(),
+            f.Commerce.Product(),
+            f.Lorem.Sentence(),
+            f.Finance.Amount(1m, 1000m)));
+    }
+}
