192e25c93e9cab89fcffa736a48e98b330f4705b
Ethan Chen <jonathanchenzheng96@gmail.com>
2023-08-28 21:16:02 -0400

    Add more entities from BeLit

---
+++ ElasticsearchClient/ElasticsearchClient/Modules/BeLit/Lot/Lot.cs
@@ -0,0 +1,16 @@
+﻿using ElasticsearchClient.Modules.BeLit.Lot.ValueObjects;
+
+namespace ElasticsearchClient.Modules.BeLit.Lot;
+
+public class Lot
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public int SubdivisionId { get; set; }
+    public int CustomerId { get; set; }
+    public int TrussManufacturerId { get; set; }
+    public LotAddress? Address { get; private set; }
+    public string? WindExposure { get; set; }
+    public string? WindSpeed { get; set; }
+    public string? ExternalLotJobNumber { get; set; }
+}
---
+++ ElasticsearchClient/ElasticsearchClient/Modules/BeLit/Lot/ValueObjects/LotAddress.cs
@@ -0,0 +1,10 @@
+﻿namespace ElasticsearchClient.Modules.BeLit.Lot.ValueObjects;
+
+public class LotAddress 
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+    public string County { get; set; }
+}
